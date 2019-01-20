4ece7e448130883e0691191ff275ac9ff72bd704
dimz <kokaruk@gmail.com>
2019-01-20 15:29:24 +1100

    Api Consumer

--- WdtA2Api/Startup.cs
+++ WdtA2Api/Startup.cs
@@ -60,6 +60,7 @@ namespace WdtA2Api
             if (env.IsProduction() || env.IsStaging() || env.IsEnvironment("Staging_2"))
             {
                 app.UseExceptionHandler("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
---
+++ WdtConsumer/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+
+using WdtConsumer.Models;
+
+namespace WdtConsumer.Controllers
+{
+    public class HomeController : Controller
+    {
+        // public IActionResult Index()
+        // {
+        //
+        //     return View();
+        // }
+
+        public async Task<IActionResult> Index()
+        {
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetStringAsync("http://kokaruk.com:59560/api/rooms");
+
+                //var customers = JsonConvert.DeserializeObject<List<Customer>>(result);
+                return View();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
