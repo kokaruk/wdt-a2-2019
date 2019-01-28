062c7c978cdd077e9619fe24ed792574336c79a0
dimz <kokaruk@gmail.com>
2019-01-28 17:13:50 +1100

    LOGIN and register with external

--- WdtApiLogin/wwwroot/js/site.js
+++ WdtApiLogin/wwwroot/js/site.js
@@ -1,6 +1,10 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Write your JavaScript code
 
-// auto dismiss alerts
+window.setTimeout(function () {
+    $(".alert").fadeTo(150, 0).slideUp(200, function () {
+        $(this).remove();
+    });
+}, 4000);
